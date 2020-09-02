bcc1fd1067c71d47b18e12a12ffee86f66954809
Sahab Singh <sahabsinghgurjar@yahoo.com>
2020-09-02 09:19:30 -0500

    secrets removed

--- src/main/java/com/sahab/ordermanagement/OrdermanagementApplication.java
+++ src/main/java/com/sahab/ordermanagement/OrdermanagementApplication.java
@@ -2,10 +2,12 @@ package com.sahab.ordermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
+@PropertySource("classpath:secrets.properties")
 public class OrdermanagementApplication {
 
 	public static void main(String[] args) {
